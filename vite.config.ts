c0eb8cb218e4dd74565c6986dff11fc7b61a36f3
ertu96 <48797125+ertu96@users.noreply.github.com>
2023-03-22 07:39:03 +0100

    envDir in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
+
   return {
     plugins: [vue(), vueJsx()],
     resolve: {
@@ -13,12 +14,13 @@ export default defineConfig(({ mode }) => {
         "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
+    envDir: "./env",
     server: {
       proxy: {
         "/api": {
           target: env.VITE_API_URL,
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
+          rewrite: (path) => path.replace(/^\/api/, "/api"),
         },
       },
     },
