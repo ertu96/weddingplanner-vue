024ef0233067875366b3c7a4eae4f013cbcd5813
ertu96 <48797125+ertu96@users.noreply.github.com>
2023-04-13 14:38:38 +0200

    scroll to top on route change

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,6 +20,9 @@ const router = createRouter({
       component: () => import("@/views/ScheduleView.vue"),
     },
   ],
+  scrollBehavior() {
+    return { top: 0 };
+  },
 });
 
 export default router;
